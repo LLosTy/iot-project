9e8e9c003804df87b17d4b6c3429aa2bb4c92c1f
LLosTy <marcojurgenjuras@gmail.com>
2024-03-29 14:10:54 +0100

    Made the graph work, still WIP tho

--- app/temps/page.js
+++ app/temps/page.js
@@ -1,8 +1,10 @@
 // pages/temps.js
 // app/temps/page.js
-// 'use client'
-import Temperature from '/models/temperature'
+'use client'
+// import Temperature from '/models/temperature'
 // import {useState} from "react";
+import { LineChart } from '@mui/x-charts/LineChart';
+
 
 
 const GetTemps = async () => {
@@ -10,15 +12,31 @@ const GetTemps = async () => {
         const res = await fetch("http://localhost:3000/api/temps", {
             cache: "no-store",
         })
+        // const testArr =[]
+        // res.forEach((i) => testArr.push(i.payload))
+        // return testArr
+
         // console.log(res)
         return res.json();
     }catch (err){
         console.log(err)
     }
 }
+// const test = (x) =>{
+//     let array = []
+//     x.forEach((i) => array.push(i.payload))
+//     console.log(array)
+//     return array
+//     // console.log("Test: ",x[0])
+// }
 export default async function TempsPage(){
     // const [temps, setTemps] = useState([])
     const {temps} = await GetTemps()
+    // const testArr = temps.forEach((i) => testArr.push(i.payload))
+    const testArr = temps.map(i => i.payload);
+    // const [ arraytemps ] = toArray(temps)
+    // let tempsArr = []
+    // tempsArr = toArray(temps)
     // setTemps(await GetTemps())
     // let temps = []
     // temps = await GetTemps();
@@ -28,11 +46,23 @@ export default async function TempsPage(){
         <div>
             <h1>Latest Temperatures</h1>
             {/*{temps}*/}
-                {temps.map((temp) => (
-                    // eslint-disable-next-line react/jsx-key
-                    <div>{temp.payload}</div>
-                    ))}
+            {/*    {temps.map((temp) => (*/}
+            {/*        // eslint-disable-next-line react/jsx-key*/}
+            {/*        <div>{temp.payload}</div>*/}
+            {/*        ))}*/}
+
+            {testArr}
+            {console.log(testArr)}
+            {/*{tempsArr}*/}
+            {/*{testArr}*/}
+            <LineChart
+                xAxis={[{ scaleType: 'point', data: Array.from(Array(testArr.length).keys())}]}
+                // series={[{data}]}
+             series={[{data: testArr}]}
+                height={400}
+            />
         </div>
+
     );
 };
 
