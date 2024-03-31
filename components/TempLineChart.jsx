22b2d5249fd98487edc5544628488d0a4833599f
LLosTy <marcojurgenjuras@gmail.com>
2024-03-31 14:21:03 +0200

    Fixed the indexes for the temps chart

--- components/TempLineChart.jsx
+++ components/TempLineChart.jsx
@@ -1,10 +1,17 @@
 import {LineChart} from "@mui/x-charts/LineChart";
 
+const getIndex = (temperatures) => {
+    let indexes = Array.from(Array(temperatures.length).keys())
+    for(let index in indexes){
+        indexes[index]++
+    }
+    return indexes
+}
 const TempLineChart = ({
     rawTemperatures
 }) => {
     const temperatures = rawTemperatures.map(i => i.payload);
-    const temperaturesIndex = Array.from(Array(temperatures.length).keys())
+    const temperaturesIndex = getIndex(temperatures)
     return(
         <LineChart
             xAxis={[{ scaleType: 'point', data: temperaturesIndex}]}
