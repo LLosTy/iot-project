c7929dc4a3cb66564fee766b190b8c896dd583e8
Ivan <ivan.bilej25@gmail.com>
2024-04-01 16:44:57 +0200

    Rewrited code so that instead of functions separated by If, we have one function where if only changes the query for filter

--- app/api/temps/route.js
+++ app/api/temps/route.js
@@ -6,47 +6,37 @@ import { URL, URLSearchParams } from 'url';
 
 export async function GET(req){
     await connectMongoDB();
-    const myUrl = new URL(req.url)
 
-    //console.log(req)
-    //console.log(req.method)
-    //console.log(req.url)
-    //console.log(new URL(req.url).searchParams.get("bla"))
-    //console.log(myUrl)
-    //console.log(myUrl.search)
-    //console.log(myUrl.searchParams)
-    //console.log(myUrl.searchParams.toString())
-    //const { dateFrom, dateTo } = myUrl.searchParams
-    //console.log(dateFrom)
-    //console.log(dateTo)
-
-    if (myUrl.searchParams.has("dateFrom") && myUrl.searchParams.has("dateTo")) {
+    try {
+        const myUrl = new URL(req.url)
         const dateFrom = myUrl.searchParams.get("dateFrom")
         const dateTo = myUrl.searchParams.get("dateTo")
-
-        try {
-            const temps = await Temperature.find({ 
-                "timestamp": {
-                    $gte: new Date(dateFrom),
-                    $lte: new Date(dateTo)
-                }
-              });
-            console.log("Temps:" + temps)
-            //return res.status(200).json({ temps });
-            return NextResponse.json({ temps })
-          } catch (error) {
-            //return res.status(500).json({ error: error.message });
-            return NextResponse.json({ error: error.message })
-          }
-    }
-
-    else {
-        try{
-            const temps = await Temperature.find().limit(20)
-
-            return NextResponse.json({temps})
-        }catch(err){
-            console.log(err)
+        const iotId = myUrl.searchParams.get("iot")
+        
+        let query = {}
+        
+        if (myUrl.searchParams.has("dateFrom") && myUrl.searchParams.has("dateTo")) {  
+            query.timestamp = {
+                $gte: new Date(dateFrom),
+                $lte: new Date(dateTo)
+            }
         }
+        else if (myUrl.searchParams.has("iot")) {
+            query.hardwareId = iotId
+        }
+
+        const temps = await Temperature.find(query)
+            .limit( 
+            (dateFrom && dateTo) || iotId ? 
+                undefined 
+            : 20
+        )
+        //console.log("Temps:" + temps)
+
+        return NextResponse.json({ temps })
+
+    } catch(error) {
+        console.log(error)
+        return NextResponse.json({ error: error.message })
     }
 }
