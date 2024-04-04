7c817563fe42c8c2b793c0b3bd66310a003034a8
LLosTy <marcojurgenjuras@gmail.com>
2024-04-04 13:17:30 +0200

    Added a date selector to page.js and some dependencies

--- app/temps/page.js
+++ app/temps/page.js
@@ -2,10 +2,17 @@
 // app/temps/page.js
 'use client'
 // import Temperature from '/models/temperature'
+import React from "react";
 import { useState, useEffect } from "react";
-import { LineChart } from '@mui/x-charts/LineChart';
+// import { LineChart } from '@mui/x-charts/LineChart';
 import { GetTemps, GetTempsByDate } from '../lib/actions'
 import TempLineChart from '../../components/TempLineChart'
+import dayjs from 'dayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
 
 
 // const test = (x) =>{
@@ -17,7 +24,7 @@ import TempLineChart from '../../components/TempLineChart'
 // }
 
 export default function TempsPage(){
-    const [temps, setTemps] = useState([])
+    const [temps, setTemps]  = useState([])
     const [temps2, setTemps2] = useState([])
     let testArr = []
     let temps2Array = []
@@ -56,7 +63,7 @@ export default function TempsPage(){
         const fetchTemps = async () => {
             const fetchedTemps = await GetTemps();
             setTemps(fetchedTemps);
-            
+
             const fetchedTemps2 = await GetTempsByDate(dateFrom, dateTo);
             console.log(fetchedTemps2)
             setTemps2(fetchedTemps2);
@@ -65,8 +72,10 @@ export default function TempsPage(){
         fetchTemps();
     }, [])
 
-    
+    const [value, setValue] = React.useState(dayjs('2022-04-17'));
+
     return (
+
         <div>
             <h1>Latest Temperatures</h1>
             {/*{temps}*/}
@@ -86,6 +95,16 @@ export default function TempsPage(){
             {/*    height={400}*/}
             {/*/>*/}
 
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={['DatePicker', 'DatePicker']}>
+                    {/*<DatePicker label="Uncontrolled picker" defaultValue={dayjs('2022-04-17')} />*/}
+                    <DatePicker
+                        label="Controlled picker"
+                        value={dateTo}
+                        onChange={dateTo}
+                    />
+                </DemoContainer>
+            </LocalizationProvider>
             <TempLineChart rawTemperatures={temps}/>
 
             
