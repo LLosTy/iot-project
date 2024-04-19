562659626b02a0fd30e9218f1bb51dd94c9e5e8b
LLosTy <marcojurgenjuras@gmail.com>
2024-04-19 08:59:36 +0200

    Added more params to DeviceCard

--- app/device/page.js
+++ app/device/page.js
@@ -4,12 +4,18 @@ import DeviceCard from '/components/DeviceCard'
 import AddDeviceCard from '/components/AddDeviceCard'
 import {Add} from "@mui/icons-material";
 import {Grid} from "@mui/material";
+//TODO fetch data based on the currently signed in user
+
 export default function DevicePage(){
     return(
         <div>
             <Grid container spacing={2} columns={16}>
                 <Grid item xs={8}>
-                    <DeviceCard/>
+                    <DeviceCard DeviceId={1234}
+                                Owner={"Marco"}
+                                Room={"Master Bedroom"}
+                                LatestTemp={25}
+                    />
                     <AddDeviceCard/>
                 </Grid>
             </Grid>
--- components/DeviceCard.jsx
+++ components/DeviceCard.jsx
@@ -1,19 +1,27 @@
 import {Card, CardActionArea, CardContent, CardMedia, Typography} from "@mui/material";
 import MemoryIcon from '@mui/icons-material/Memory';
-export default function DeviceCard() {
+
+const DeviceCard = ({
+                DeviceId,
+                Owner,
+                Room,
+                LatestTemp
+                    }) => {
     return (
         <Card sx={{ maxWidth: 345 }}>
             <CardActionArea>
                 {/*<CardMedia><MemoryIcon/></CardMedia>*/}
                 <CardContent>
                     <Typography gutterBottom variant="h6" component="div">
-                       <p>Device ID:</p>
-                        <p>Owner:</p>
-                        <p>Room:</p>
-                        <p>Latest Temp:</p>
+                       <p>Device ID: {DeviceId}</p>
+                        <p>Owner: {Owner}</p>
+                        <p>Room:{Room}</p>
+                        <p>Latest Temp:{LatestTemp}</p>
                     </Typography>
                 </CardContent>
             </CardActionArea>
         </Card>
     )
 }
+
+export default DeviceCard
