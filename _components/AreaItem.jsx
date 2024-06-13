19f821642a4fde947c8adf113b913e7ab06a22b3
LLosTy <marcojurgenjuras@gmail.com>
2024-06-13 20:08:56 +0200

    Made slight changes to options.ts and started on AreaItem component

--- _components/AreaItem.jsx
+++ _components/AreaItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, CardActionArea, CardContent, CardMedia, Typography} from "@mui/material";
+import {Card, CardActionArea, CardContent, Typography} from "@mui/material";
 
 const AreaItem = ({
                       id,
